d07cdd78e874db50ee6500f519e7937b7c4b9bfc
Haider Khan <hkhan@datalofts.com>
2019-06-04 01:29:42 -0700

    Initial Commit

---
+++ SwiftUIWrapper/SwiftUIWrapper.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUIWrapper.h
+//  SwiftUIWrapper
+//
+//  Created by Haider Khan on 6/4/19.
+//  Copyright © 2019 zkhaider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftUIWrapper.
+FOUNDATION_EXPORT double SwiftUIWrapperVersionNumber;
+
+//! Project version string for SwiftUIWrapper.
+FOUNDATION_EXPORT const unsigned char SwiftUIWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIWrapper/PublicHeader.h>
+
+
